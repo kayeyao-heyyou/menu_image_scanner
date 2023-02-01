1f247c83ac9850674ec936ca9edc373ecdd98eee
kayeyao-heyyou <121843591+kayeyao-heyyou@users.noreply.github.com>
2023-02-01 14:51:07 +0800

    Update menu_image_scanner.py

--- menu_image_scanner.py
+++ menu_image_scanner.py
@@ -256,6 +256,17 @@ with tab1:
     uploaded_files2 = copy.deepcopy(uploaded_files)
 
     if uploaded_files is not None:
+        for uploaded_file in uploaded_files2:
+            menu_raw, menu_clean = convert_to_text(file=uploaded_file)
+            menu_raw.insert(0,'source',uploaded_file.name)
+            menu_clean.insert(0,'source',uploaded_file.name)
+            menu_raw_all = pd.concat([menu_raw_all, menu_raw])
+            menu_clean_all = pd.concat([menu_clean_all, menu_clean])
+            menu_clean_all = menu_clean_all.fillna("-")
+            st.session_state.result1 = menu_raw_all
+            st.session_state.result2 = menu_clean_all
+	
+	
         try:
             for i in range(0, len(uploaded_files)//5+1):
                 cols = st.columns(5)
@@ -275,16 +286,6 @@ with tab1:
 	    
             st.header("")
             st.subheader("Convert Image / PDF to text:")
-	
-	    for uploaded_file in uploaded_files2:
-                menu_raw, menu_clean = convert_to_text(file=uploaded_file)
-                menu_raw.insert(0,'source',uploaded_file.name)
-                menu_clean.insert(0,'source',uploaded_file.name)
-                menu_raw_all = pd.concat([menu_raw_all, menu_raw])
-                menu_clean_all = pd.concat([menu_clean_all, menu_clean])
-                menu_clean_all = menu_clean_all.fillna("-")
-                st.session_state.result1 = menu_raw_all
-                st.session_state.result2 = menu_clean_all
 
             if st.button("Convert to Text", key=1):
                 st.header("")
