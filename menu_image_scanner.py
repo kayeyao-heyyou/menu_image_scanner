ae976645428a07912f4c3d17c86a310a17b9331e
kayeyao-heyyou <121843591+kayeyao-heyyou@users.noreply.github.com>
2023-02-01 16:44:59 +0800

    Update menu_image_scanner.py

--- menu_image_scanner.py
+++ menu_image_scanner.py
@@ -222,7 +222,7 @@ def convert_df(df):
 
 def show_pdf(file_path):
     base64_pdf = base64.b64encode(file_path.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="400" height="250" type="application/pdf">' 
+    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="400" height="250" type="application/pdf">' 
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 
